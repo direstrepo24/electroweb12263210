940e427f15e097df1ed38446cdd16e3679dc6fe9
enu-44 <enuarmunozcastillo@gmail.com>
2018-02-09 10:09:01 -0500

    Changes Program

--- Program.cs
+++ Program.cs
@@ -4,12 +4,14 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 
 namespace electroweb
 {
     public class Program
     {
+        /* 
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
@@ -18,6 +20,39 @@ namespace electroweb
                     options.Limits.MinResponseDataRate = null;
                     options.Limits.MaxRequestBodySize=null;
                    
+                })
+                //.UseWebRoot("http://54.86.105.4/wwwroot")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5001/")
+                .ConfigureAppConfiguration((builderContext, config) =>
+                {
+                IHostingEnvironment env = builderContext.HostingEnvironment;
+
+                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                })
+                .Build();
+
+            host.Run();
+        }*/
+
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel(options => 
+                { 
+                    options.Limits.MinResponseDataRate = null;
+                    options.Limits.MaxRequestBodySize=null;
+                   options.Limits.MaxConcurrentConnections = 100;
+    options.Limits.MaxConcurrentUpgradedConnections = 100;
+   
+    options.Limits.MinRequestBodyDataRate =
+        new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+    options.Limits.MinResponseDataRate =
+        new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+    //options.Listen(IPAddress.Loopback, 5000);
                 })
                 //.UseWebRoot("http://54.86.105.4/wwwroot")
                 .UseContentRoot(Directory.GetCurrentDirectory())
